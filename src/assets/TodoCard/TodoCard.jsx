c0d1b9d8625a2e17cc30488f10ddd6eaa7d86516
Giorgi Cxovrebashvili <giorgicxovrebashvili@Giorgis-MacBook-Air.local>
2024-03-09 16:11:38 +0400

    small change

--- src/assets/TodoCard/TodoCard.jsx
+++ src/assets/TodoCard/TodoCard.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
+import React from "react";
 
-const TodoCard = ({todoId, checkTodoFunction, todo, deletTodoFunction}) => {
+const TodoCard = ({ todoId, checkTodoFunction, todo, deletTodoFunction }) => {
   return (
-    <div className='ul'>
-        <div style={{display: 'flex', gap: '20px', justifyContent: 'space-between', alignItems: 'center'}} key={todoId}>
-              <input
-                  className='checkbox'
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => checkTodoFunction(todoId)} />
-              <li
-                  className='task'
-                  style={{ listStyle: 'none', textDecoration: todo.completed ? 'line-through' : 'none', color: todo.completed ? 'grey' : 'black', width: '310px' }}>{todo.text}
-              </li>
-              <button
-                  className='btn'
-                  onClick={() => deletTodoFunction(todo.text)}
-                  style={{cursor: 'pointer'}}
-              >
-                  X
-              </button>
-        </div>
+    <div className="ul">
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+        key={todoId}
+      >
+        <input
+          className="checkbox"
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => checkTodoFunction(todoId)}
+        />
+        <li
+          className="task"
+          style={{
+            listStyle: "none",
+            textDecoration: todo.completed ? "line-through" : "none",
+            color: todo.completed ? "grey" : "black",
+            width: "310px",
+          }}
+        >
+          {todo.text}
+        </li>
+        <button
+          className="btn"
+          onClick={() => deletTodoFunction(todo.text)}
+          style={{ cursor: "pointer" }}
+        >
+          X
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TodoCard
+export default TodoCard;
